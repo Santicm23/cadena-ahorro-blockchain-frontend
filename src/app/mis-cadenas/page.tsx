b5cfef43cbc9dbbfac87b4c208a45f51f9d1dc8b
Santicm23 <santicastrom@outlook.com>
2024-05-24 08:39:42 -0500

    starting frontend

---
+++ src/app/mis-cadenas/page.tsx
@@ -0,0 +1,7 @@
+export default function MisCadenasPage() {
+  return (
+    <main className="h-screen w-screen grid">
+      <h1 className="z-10 self-center justify-self-center text-4xl text-slate-200">No ha creado cadenas</h1>
+    </main>
+  );
+}
